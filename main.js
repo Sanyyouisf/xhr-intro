5e14bc778997f474867597b2aa4772224280456f
Sanyyouisf <sanymamdoh@yahoo.com>
2017-03-06 20:01:56 -0600

    add jason request

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 console.log("hello");
+function executeThisCodeAfterLoading(){
+	console.log("Loading");
+};
+
+function executeThisCodeAfterFileFails(){
+	console.log("Fail");
+};
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener ("load", executeThisCodeAfterLoading);
+myRequest.addEventListener ("error", executeThisCodeAfterFileFails);
+myRequest.open("GET","dionsaur.json");// get the data from the ginosaure 
+myRequest.send(); //send to execute the request
+console.log("myRequest",myRequest)
+	
